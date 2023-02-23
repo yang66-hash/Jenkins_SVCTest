b2d8a7a44b745787407bca8589dc508d0fd5d5f9
yang66-hash <2019023491@qq.com>
2023-02-23 19:01:08 +0800

    war packaging

--- src/main/java/com/yang/demo/DemoApplication.java
+++ src/main/java/com/yang/demo/DemoApplication.java
@@ -2,12 +2,16 @@ package com.yang.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class DemoApplication {
-
+public class DemoApplication extends SpringBootServletInitializer {
     public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
+        SpringApplication.run(DemoApplication.class);
     }
-
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(DemoApplication.class);
+    }
 }
