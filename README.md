595ba0cf785f71acf2861140ea34ee6f39d77fc9
yang66-hash <2019023491@qq.com>
2023-02-23 18:46:36 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# 工程简介
+A Simple SpringBoot Demo used for testing Jenkins
+
+
---
+++ src/main/java/com/yang/demo/controller/RouteController.java
@@ -0,0 +1,23 @@
+package com.yang.demo.controller;
+
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class RouteController {
+    @ResponseBody
+    @GetMapping("/getRouteInfo")
+    public Map<String,String> getRouteInfo(@RequestParam(value = "userId") String userId, @RequestHeader HttpHeaders httpHeaders){
+        Map<String,String> result = new HashMap<>();
+
+        result.put("from","威海");
+        result.put("to","上饶");
+        result.put("flight", "MU5542");
+
+        return result;
+    }
+}
