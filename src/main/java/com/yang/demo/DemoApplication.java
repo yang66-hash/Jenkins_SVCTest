a16ffc69b340a8d5afb482faef744fa07c03d88b
yang66-hash <2019023491@qq.com>
2023-02-27 15:52:16 +0800

    make dockerfile and use package in jar

--- src/main/java/com/yang/demo/DemoApplication.java
+++ src/main/java/com/yang/demo/DemoApplication.java
@@ -10,8 +10,5 @@ public class DemoApplication extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class);
     }
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-        return builder.sources(DemoApplication.class);
-    }
+
 }
