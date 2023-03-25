05e7154b2c2b96319b67cd37d8790e3f1ac1bb5a
yang66-hash <2019023491@qq.com>
2023-03-25 22:01:24 +0800

    一些修改

--- src/main/java/com/yang/demo/controller/RouteController.java
+++ src/main/java/com/yang/demo/controller/RouteController.java
@@ -1,6 +1,7 @@
 package com.yang.demo.controller;
 
 
+import com.yang.demo.JenkinsConnect;
 import org.springframework.http.HttpHeaders;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,14 +13,9 @@ public class RouteController {
     @ResponseBody
     @GetMapping("/getRouteInfo")
     public Map<String,String> getRouteInfo(@RequestParam(value = "userId") String userId, @RequestHeader HttpHeaders httpHeaders){
-        Map<String,String> result = new HashMap<>();
 
-        result.put("from","威海");
-        result.put("to","上饶");
-        result.put("flight", "MU5542");
-        result.put("client","zhangsan");
-        result.put("cost","1000$");
+        JenkinsConnect.connection();
 
-        return result;
+        return null;
     }
 }
